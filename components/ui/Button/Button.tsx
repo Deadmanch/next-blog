9c77d04b9b59c07be3b46a3dc959570abaa4b0da
Deadmanch <deadmanch@MBP-Danil.IGD_Rostelecom>
2024-09-28 03:30:02 +0300

    feature - создал компоненты Input и TextArea

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -1,15 +1,19 @@
 import { ButtonHTMLAttributes, forwardRef } from 'react';
 
+import { clsx } from 'clsx';
+
 import styles from './Button.module.css';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, ...props }, ref) => {
-	return (
-		<button ref={ref} className={styles.button} {...props}>
-			{children}
-		</button>
-	);
-});
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+	({ children, className, ...props }, ref) => {
+		return (
+			<button ref={ref} className={clsx(styles.button, className)} {...props}>
+				{children}
+			</button>
+		);
+	}
+);
 
 Button.displayName = 'Button';
